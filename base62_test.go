d1fc3ccef6c2d88aea71139a9be7420e29174a57
Alex Tan Hong Pin <alextan220990@gmail.com>
2018-11-08 19:19:30 +0800

    docs: update test

--- base62_test.go
+++ base62_test.go
@@ -2,6 +2,7 @@ package base62_test
 
 import (
 	"log"
+	"math/rand"
 	"testing"
 	"testing/quick"
 
@@ -15,5 +16,30 @@ func TestBase62(t *testing.T) {
 
 	if err := quick.Check(f, nil); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func TestCompare(t *testing.T) {
+	f := func(i uint64) bool {
+		return Encode(i) == EncodePerf(i)
+	}
+
+	if err := quick.Check(f, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// go test -bench=. -benchmem -cpuprofile cpu.out -memprofile mem.out
+func BenchmarkEncode(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	for i := 0; i < b.N; i++ {
+		Encode(r.Uint64())
+	}
+}
+
+func BenchmarkEncodePerf(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	for i := 0; i < b.N; i++ {
+		EncodePerf(r.Uint64())
 	}
 }
